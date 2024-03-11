6f3dd2a0fe6edfbde1bcba7e7823562c77d48c02
Johann Sylvester J <johannsylvester04@gmail.com>
2024-03-12 01:42:32 +0530

    Last fix directory missed.

---
+++ mysql_Server/init.sql
@@ -0,0 +1,47 @@
+create database if not exists attendance_management;
+
+use attendance_management;
+
+drop table if exists student;
+drop table if exists subject;
+drop table if exists teacher;
+drop table if exists teacher_sub;
+drop table if exists attendance;
+drop table if exists period;
+
+
+create table student(
+email varchar(100) primary key,
+st_name varchar(20),
+class varchar(10)
+);
+
+create table subject(
+sub_code varchar(10) primary key,
+sub_name varchar(40)
+);
+
+create table teacher(
+teach_id varchar(10) primary key,
+teacher_name varchar(20)
+);
+
+create table teacher_sub(
+Teach_id varchar(10),
+sub_code varchar(10),
+class varchar(10)
+);
+
+create table attendance(
+s_no integer primary key auto_increment,
+email varchar(100),
+sub_code varchar(10),
+prd integer,
+attd_date date
+);
+
+create table period(
+prd integer primary key,
+st_time time,
+end_time time
+);
