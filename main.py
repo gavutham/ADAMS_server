18dc872ad3418401d17042d9fb5341d5dc1870fd
Johann Sylvester J <johannsylvester04@gmail.com>
2024-04-15 20:34:07 +0530

    Fix return val of post-beacon-details.

--- main.py
+++ main.py
@@ -91,7 +91,7 @@ def is_session_started(year, dep, sec):
 @app.route("/post-beacon-details/<ip>/<classroom>", methods=["POST"])
 def post_beacon_details(ip, classroom):
     beacons_col.insert_one({"ip": ip, "classroom": classroom})
-    return 200
+    return "Done!", 200
 
 
 @app.route("/pp-status-verify/<year>/<dep>/<sec>/<email>")
