cec417ba4af05e186785fb3c71b1bf31505178ce
Johann Sylvester J <johannsylvester04@gmail.com>
2024-08-29 01:03:53 +0530

    db migration overnight

--- main.py
+++ main.py
@@ -3,20 +3,12 @@
 import requests
 from flask import Flask, request, make_response, jsonify
 from fire_base import firebase_server
-from fire_base.firebase_server import set_attendance_data, set_attendance_flag
+from fire_base.firebase_server import set_attendance_data, set_attendance_flag, is_session_started, initiate_session, get_session_students, update_session_students, get_session_details, get_session_details_with_classname, update_session_ips, get_session_ips
 from mark_attendance import mark_attendance
 from mongo import mongo
-from pymongo import MongoClient
 import threading 
 import time
 
-
-client = MongoClient('localhost', 27017)
-
-db = client["adams_server_db"]
-sessions_col = db["sessions"]
-beacons_col = db["beacons"]
-pp_status_col = db["pp_status"]
 
 app = Flask(__name__)
 
@@ -25,44 +17,48 @@ app = Flask(__name__)
 def hello_world():
     return "Hello world!"
 
-
-@app.route("/<year>/<dep>/<sec>", methods=["GET"])
-def get_students(year, dep, sec):
-    return mongo.get_session_students(sessions_col, year, dep, sec), 200
-
-
+#done
 @app.route("/get-student-uuid/<year>/<dep>/<sec>/<email>", methods=["GET"])
 def get_student_uuid(year, dep, sec, email):
-    std_data = mongo.get_session_students(sessions_col, year, dep, sec)
+    std_data = get_session_students(year, dep, sec)
+    updated_data = []
+    uuid = ""
+
     if not(std_data):
         return 403
     else:
         for i in std_data:
             if i["email"] == email:
-                sessions_col.update_one({"email": email}, {"$set": {"ready": True}})
-                return str(i["uuid"]), 200
+                uuid = i["uuid"]
+                i["ready"] = True
+                updated_data.append(i)
+            else:
+                updated_data.append(i)
+
+    if uuid == "":
         return "Student not found!", 404
 
+    update_session_students(year, dep, sec, updated_data)
+    return uuid, 200
 
+#done
 # Top level function - TEACHER APP
 @app.route("/start-session/<year>/<dep>/<sec>")
 def start_session(year, dep, sec):
     timeout_thread = threading.Thread(target=stop_session_thread, args=(year, dep, sec))
     timeout_thread.start()
-    if mongo.is_session_started(sessions_col, year, dep, sec):
+
+    if is_session_started(year, dep, sec):
         return "Session already started!", 404
+      
     session_students = firebase_server.generate_sec_uuids(year, dep, sec)
-    sessions_col.insert_many(session_students)
-    mark_attendance.wait_for_login_threshold(year, dep, sec, sessions_col) # Waits until 80% threshold reached.
-    # mark_attendance.pp_start_session(year, dep, sec)
-    ready_uuids = [i["uuid"] for i in
-                   mongo.get_session_students(sessions_col, year, dep, sec)
-                   if (i["ready"]) == True] # Only ready.
-    return jsonify(ready_uuids), 200
-    # return "Done!", 200
-    # return at_dict # for testing
+    initiate_session(year, dep, sec, session_students)
 
+    set_attendance_flag(year, dep, sec, True)  
+    
+    return "Success", 200
 
+#done
 def stop_session_thread(year, dep, sec):
     time.sleep(180)
     stop_session(year, dep, sec)
@@ -70,14 +66,14 @@ def stop_session_thread(year, dep, sec):
 
 @app.route("/stop-session/<year>/<dep>/<sec>")
 def stop_session(year, dep, sec):
-    sessions_col.update_many({"pp_verify": True}, {"$set": {"att_verified": True}})
-    if mongo.is_session_started(sessions_col, year, dep, sec):
-        std_data = mongo.get_session_students(sessions_col, year, dep, sec)
+
+    if is_session_started(year, dep, sec):
+        std_data = get_session_students(year, dep, sec)
         print("\nSTOPPING SESSION!\n")
         present = []
         absent = []
         for i in std_data:
-            if i["att_verified"]:
+            if i["pp_verify"]:
                 present.append(i["email"])
             else:
                 absent.append(i["email"])
@@ -86,7 +82,6 @@ def stop_session(year, dep, sec):
         print(present)
         print("\nABSENT\n")
         print(absent)
-        sessions_col.delete_many({"year": year, "department": dep, "section": sec})
 
         set_attendance_data(year, dep, sec, present, absent) #store the result in firebase
         set_attendance_flag(year, dep, sec, False)
@@ -95,9 +90,10 @@ def stop_session(year, dep, sec):
     else:
         return "Session not started yet!", 404
 
+#done
 @app.route("/is-session-started/<year>/<dep>/<sec>")
-def is_session_started(year, dep, sec):
-    if mongo.is_session_started(sessions_col, year, dep, sec):
+def is_session_start(year, dep, sec):
+    if is_session_started(year, dep, sec):
         return "true";
     else:
         return "false";
@@ -105,16 +101,26 @@ def is_session_started(year, dep, sec):
 
 @app.route("/post-beacon-details/<ip>/<classroom>", methods=["POST"])
 def post_beacon_details(ip, classroom):
-    beacons_col.replace_one({"ip": ip}, {"ip": ip, "classroom": classroom}, upsert=True) # init beacon.
+    ips = []
+    try:
+        ips = get_session_details_with_classname(classroom)["ips"]
+    except KeyError:
+        ips = []
+    ips.append(ip)
+
+    update_session_ips(classroom, ips)
+
     return "Done!", 200
 
 
 @app.route("/pp-status-verify/<year>/<dep>/<sec>/<email>")
 def pp_status_verify(year, dep, sec, email):
-    if not mongo.is_session_started(sessions_col, year, dep, sec):
+    if not is_session_started(year, dep, sec):
         return "Session not started!", 403
     else:
-        student = sessions_col.find_one({"year": year, "department": dep, "section": sec, "email": email})
+        students = get_session_students(year, dep, sec)
+        student = list(filter(lambda x: x["email"] == email, students))[0]
+        
         if student["pp_verify"] == True:
             return "true", 200
         else:
@@ -125,14 +131,12 @@ def pp_status_verify(year, dep, sec, email):
 @app.route("/pp-verify/<year>/<dep>/<sec>", methods=["POST"])
 def pp_verify(year, dep, sec):
     yds = year+dep+sec
-    if not mongo.is_session_started(sessions_col, year, dep, sec):
+    if not is_session_started(year, dep, sec):
         return "Session not started!", 403
     req = request.get_json()
-    pp_status_col.insert_one({"classroom": yds}, {"classroom": yds, "scan_details": req})
-    print(req)
 
     req_pp_list = list(req) # list of dictionaries with keys uuid and rssi parsed from request.
-    std_data = mongo.get_session_students(sessions_col, year, dep, sec)
+    std_data = get_session_students(year, dep, sec)
     std_uuids = [i["uuid"] for i in std_data]
     print("STD UUIDS:")
     print(std_uuids)
@@ -143,58 +147,85 @@ def pp_verify(year, dep, sec):
 
     count = 0;
     pp_top = []
+
+    updated_students = []
+
     for i in pp_sorted:
-        # att_verified = True is hardcoded. Should happen only after both bb-verify and face-auth is done.
-        # sessions_col.update_one({"uuid": i["uuid"]}, {"$set": {"pp_verify": True, "att_verified": True}})
-        result = sessions_col.update_one({"uuid": i["uuid"], "bb_rssi": {"$lt": i["rssi"]}}, {"$set": {"pp_verify": True}})
-        sessions_col.update_one({"uuid": i["uuid"], "pp_rssi": {"$lt": i["rssi"]}}, {"$set": {"pp_rssi": i["rssi"]}})
-        if result.matched_count > 0:
-            pp_top.append(i)
+        student = list(filter(lambda x: x["uuid"] == i["uuid"], std_data))[0]
+
+        updated = False
+
+        if (student["bb_rssi"] < i["rssi"] and count < 2):
+            updated = True
+            student["pp_verify"] = True
             count += 1
-            if count >= 2:
-                break;
 
-    # for i in pp_sorted:
-    #     sessions_col.update_one({"uuid": i["uuid"], "pp_rssi": {"$gt": i["rssi"]}}, {"$set": {"pp_rssi": i["rssi"]}})
-    # pp_list[year+dep+sec].update(req_pp_list)
-    # Set pp_verify = True for closest 5 students.
+        if (student["pp_rssi"] < i["rssi"]):
+            updated = True
+            student["pp_rssi"] = i["rssi"]
+        
+        if (updated):
+            updated_students.append(student)
+
+    
+    data_to_write = []
+
+    for stud in std_data:
+        student = list(filter(lambda x: x["uuid"] == stud["uuid"], updated_students))
+        if(len(student) > 0):
+            data_to_write.append(student[0])
+        else:
+            data_to_write.append(stud)
+            
+    
+
+    update_session_students(year, dep, sec, data_to_write)
+
     return pp_top, 200
-
-
-def pp_avg_rssi(year, dep, sec):
-    yds = year+dep+sec
-    pp_lists = pp_status_col.find({"classroom": yds})
 
 
 @app.route("/get-beacon-ips/<year>/<dep>/<sec>", methods=["GET"])
 def get_beacon_ips(year, dep, sec):
-    yds = year+dep+sec
-    ips = [i["ip"] for i in beacons_col.find({"classroom": {"$regex": yds}})]
-    # print(ips)
+    ips = get_session_ips(year, dep, sec)
     return jsonify(ips)
-    # for ip in ips:
-        # resp = requests.get("http://"+ip+"/ble_scan")
-        # print(str(resp))
-
+ 
 
 @app.route("/bb-verify/<year>/<dep>/<sec>", methods=["POST"])
 def bb_verify(year, dep, sec):
+
     req = request.get_json()
-    # req = json.loads(req)
     print(req)
-    # return "Done!", 200
+
+    std_data = get_session_students(year, dep, sec)
+
+
+    updated_students = []
+
     for scan in req:
         scan_results = scan["scan_results"]
         for device in scan_results:
             print(device)
-            sessions_col.update_one({"uuid": device["uuid"].lower(), "bb_rssi": {"$lt": device["rssi"]}}, {"$set": {"bb_rssi": device["rssi"]}}) # If beacon says too close.
-            # sessions_col.update_one({"uuid": device["uuid"].lower(), "$and": [{"pp_rssi": {"$lt": device["rssi"]}}, {"pp_rssi": {"$ne": float('inf')*-1}}]}, {"$set": {"bb_verify": False}})
-            # print("BB VERIFY (FALSE): ", device["uuid"].lower())
+            student = list(filter(lambda x: x["uuid"] == device["uuid"].lower(), std_data))[0]
+            
+            if (student["bb_rssi"] < device["rssi"]):
+                student["bb_rssi"] = device["rssi"]
+                updated_students.append(student)
+
+    data_to_write = []
+
+    for stud in std_data:
+        student = list(filter(lambda x: x["uuid"] == stud["uuid"], updated_students))
+        if(len(student) > 0):
+            data_to_write.append(student[0])
+        else:
+            data_to_write.append(stud)
+            
+
+    update_session_students(year, dep, sec, data_to_write)\
+    
     print("Req: ", req)
     return "Done!", 200
 
 
-
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080, debug=True)
-    # app.run(host='144.91.106.164', port=8000, debug=True)
